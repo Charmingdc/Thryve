9d4c703f39fc9931e8e3a34223fc9865aeb03013
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2024-12-27 22:34:14 +0000

    continue login page and worked on some SEO

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './LoginPage.css';
 
 
@@ -10,7 +11,10 @@ const LoginPage = () => {
 
         <div className="login-nav">
           <div> Login </div>
-          <div> Signup </div>
+
+          <div>
+           <Link to='/signup'> Signup </Link>
+          </div>
         </div>
 
         <div className="group">
@@ -19,14 +23,15 @@ const LoginPage = () => {
 
         <div className="group">
           <svg stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="icon">
-                     <path d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" stroke-linejoin="round" stroke-linecap="round"></path>
-                             </svg>
+            <path d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" stroke-linejoin="round" stroke-linecap="round"></path>
+          </svg>
+
           <input className="input" type="password" name="password" placeholder="Enter your password" />
         </div>
 
-        <a id="forgot-password">
-          Ohh, I forgot my password 🔐
-        </a>
+        <Link to='/reset-password' id="forgot-password">
+          Forgot password ?
+        </Link>
 
         <button className='login-button'>
           Login
