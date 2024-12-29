424cf1bf4931350a4059a5e17f605acf2917393c
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2024-12-29 15:00:52 +0000

    updated landing page info to fit a gratitude journal

--- src/components/LandingPage/HeroSection/HeroSection.jsx
+++ src/components/LandingPage/HeroSection/HeroSection.jsx
@@ -6,9 +6,9 @@ const HeroSection = () => {
     return (
         <section className="hero-section">
             <div className="description">
-                <h1>The Ultimate digital <b style={{color: "#8a2be2"}}>Journal</b></h1>
+                <h1>The Ultimate gratitude <b style={{color: "#8a2be2"}}>Journal</b></h1>
                 <p>
-                 Express, reflect, and immortalize the moments of your daily life with the world’s most cherished digital journal.          . 
+                  Express, reflect, and cherish the moments of gratitude in your daily life with the world's most beloved gratitude journal.
                 </p>
                 <Link to='/signup'> Get Started </Link>
             </div>
