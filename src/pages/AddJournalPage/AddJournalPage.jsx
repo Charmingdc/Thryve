5d7eb1cbf778ba4a5fda894858ff20ef3c2f586c
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2024-12-31 13:56:35 +0000

    started add journal page

--- src/pages/AddJournalPage/AddJournalPage.jsx
+++ src/pages/AddJournalPage/AddJournalPage.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import Topnavbar from '../../components/Helpers/Topnavbar';
 import SideBar from '../../components/Helpers/SideBar';
 import './Style.css';
 
 
 const AddJournalPage = () => {
+  const [titleInput, setTitleInput] = useState('');
+  const [mainInput, setMainInput] = useState('');
   return (
    <>
     <main className='add-journal-container'>
@@ -11,6 +14,17 @@ const AddJournalPage = () => {
 
       <div className='add-journal-page'>
         <Topnavbar currentPageName='New Journal' />
+
+        <input 
+          className='title-input' 
+          type='text'
+          placeholder='Untitled'
+          value={titleInput} />
+        
+        <textarea 
+          className='main-input'
+          placeholder='Write your journal here'
+          value={mainInput} />
       </div>
     </main>
    </>
