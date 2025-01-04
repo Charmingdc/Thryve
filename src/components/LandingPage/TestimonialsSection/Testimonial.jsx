296549dc831de8032064625bc4f6a2ca922db33d
Alqudusy <yusufabdulqudus02@gmail.com>
2025-01-04 17:57:29 +0100

    Fixed the testimonial section in the landing page

--- src/components/LandingPage/TestimonialsSection/Testimonial.jsx
+++ src/components/LandingPage/TestimonialsSection/Testimonial.jsx
@@ -1,16 +1,16 @@
 const Testimonial = ({ text, author, image, title }) => {
   return (
     <div className="testimonial">
-      <div><p className="testimonial-text">{text}</p></div>
+      <p className="testimonial-text">{text}</p>
       <div className="testimonial-author">
-        <img src={image}/>
-        <div style={{marginLeft: "10px", display: "flex", flexDirection: "column"}}>
-            <span><b>{author}</b></span>
-            <span>{title}</span>
+        <img src={image} alt={`${author}'s avatar`} />
+        <div style={{ marginLeft: "10px", display: "flex", flexDirection: "column" }}>
+          <span><b>{author}</b></span>
+          <span>{title}</span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Testimonial;
