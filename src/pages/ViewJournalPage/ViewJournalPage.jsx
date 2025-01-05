7b3b1f6270e1654eb4a0d8b29319e9a307ba29be
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2025-01-05 18:29:37 +0000

    completed delete journal functionality

--- src/pages/ViewJournalPage/ViewJournalPage.jsx
+++ src/pages/ViewJournalPage/ViewJournalPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { db } from '../../firebase/firebase-init';
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
+import { toast } from "sonner";
+
 import getDateDetails from '../../functions/getDateDetails';
 import journalTime from '../../functions/journalTime';
+import getMoodPicture from '../../functions/getMoodPicture';
+
 import { FaChevronLeft } from "react-icons/fa6";
 import { HiDotsVertical } from "react-icons/hi";
-import { HiFaceSmile } from "react-icons/hi2";
 
+import Loader from '../../components/Helpers/Loader';
 import SideBar from "../../components/Helpers/SideBar";
 import "./ViewJournalPage.css";
 
@@ -15,28 +19,66 @@ import "./ViewJournalPage.css";
 const ViewJournalPage = () => {
     const [journalDetails, setJournalDetails] = useState({});
     const [fullDate, setFullDate] = useState('');
+    const [moodIcon, setMoodIcon] = useState('');
     const [time, setTime] = useState('');
     const [showOptions, setShowOptions] = useState(false);
+    const [loading, setLoading] = useState(true);
     const hasFetched = useRef(false);
     const params = useParams();
-  
-    const getJournal = async () => {
+    const navigate = useNavigate();
+
+
+    const handleJournalDelete = async () => {
+      const toastId = toast.loading('Deleting your journal...');
+
       try {
-        setLoading(true);
+        // get a reference to current journal
         const journalRef = doc(db, 'journals', params.journalId);
 
+        // call the delete function 
+        await deleteDoc(journalRef);
+
+        // show success toast
+        toast.success('Journal deleted successfully');
+
+        // redirect back home
+        navigate('/home');
+      } catch (err) {
+        console.error(err.message);
+      } finally {
+        toast.dismiss(toastId);
+      }
+    };
+
+
+    const getJournal = async () => {
+      setLoading(true);
+
+      try {  
+        // get a reference to the journal selected
+        const journalRef = doc(db, 'journals', params.journalId);
+
+        // get journal data
         const docSnap = await getDoc(journalRef);
         const docData = docSnap.data();
 
+        // transform journal mood to journal icons
+        const moodIconUrl = getMoodPicture(docData.journalMood);
+
+        // get and format date and time 
         const date = new Date(docData.createdAt.toDate());
         const formatedTime = journalTime(date);
         const dateDetails = getDateDetails(date);
 
+        // set all necessary states
+        setMoodIcon(moodIconUrl);
         setFullDate(`${dateDetails.monthName} ${dateDetails.day}, ${dateDetails.year}`);
         setTime(formatedTime);
         setJournalDetails(docData);
       } catch (err) {
         console.error(err.message);
+      } finally {
+        setLoading(false);
       }
     };
     
@@ -54,8 +96,8 @@ const ViewJournalPage = () => {
 
             <div className="view-journal-container">
                 <nav className="nav-something">
-                    <div className='topnav-icon-holder'>
-                      <FaChevronLeft className='icon' onClick={() => navigate(-1)} />
+                    <div className='topnav-icon-holder' onClick={() => navigate(-1)}>
+                      <FaChevronLeft className='icon' />
                     </div>
                     <h1> Journal Detail </h1>
                     <HiDotsVertical className="ellipsis-icon" size={24} onClick={() => setShowOptions(prevState => !prevState)} />
@@ -63,15 +105,18 @@ const ViewJournalPage = () => {
                     {showOptions && (
                         <div className="options-menu">
                             <button className="option-button" style={{color: "var(--text-color)"}}>Edit</button>
-                            <button className="option-button" style={{color: "var(--text-color)"}}>Delete Journal</button>
+                            <button className="option-button" style={{color: "var(--text-color)"}} onClick={handleJournalDelete}> Delete Journal </button>
                         </div>
                     )}
               </nav>
 
-                <div className="container">
+              { loading ? (
+                <div style={{height: '80%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '1rem'}}> <Loader /> <p> Loading.. </p> </div>
+              ) : ( 
+               <div className="container">
                   <div className="view-journal-header">
                     <div className="mood-icon-holder">
-                        <HiFaceSmile className='mood-icon' size={50} />
+                      <img src={moodIcon} alt={moodIcon} className="mood-icon" />
                     </div>
 
                     <p className="feeling"> {journalDetails.journalMood} </p>
@@ -81,7 +126,7 @@ const ViewJournalPage = () => {
                         <span>-</span>
                         <span> {time} </span>
                     </div>
-
+   
                     <h3 className="title">
                       { journalDetails.journalTitle }
                     </h3>
@@ -93,6 +138,7 @@ const ViewJournalPage = () => {
                     </p>
                   </div>
                </div>
+              )}  
             </div> 
         </div>
     )
