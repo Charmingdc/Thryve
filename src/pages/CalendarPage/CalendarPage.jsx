d7e12dfac21efbee36ead0b69f5c01176de5af36
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2025-01-06 09:37:02 +0000

    added calendar

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,3 +1,10 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from "@fullcalendar/daygrid"; // DayGrid view plugin
+import timeGridPlugin from "@fullcalendar/timegrid"; // TimeGrid plugin (week/day view)
+import interactionPlugin from "@fullcalendar/interaction"; // Interaction plugin (dragging, date click)
+import "@fullcalendar/common/main.css"; // Core CSS
+// import "@fullcalendar/timegrid/main.css";
+
 import SideBar from '../../components/Helpers/SideBar';
 import Topnavbar from '../../components/Helpers/Topnavbar';
 import BottomNav from '../../components/Helpers/BottomNav';
@@ -12,8 +19,18 @@ const CalendarPage = () => {
 
        <div className='calendar-page'>
          <Topnavbar currentPageName='calendar' />
-         
-         <div className='calendar-wrapper'></div>
+        
+         <div className='calendar-wrapper'>
+          <FullCalendar 
+           plugins={[dayGridPlugin, interactionPlugin]}
+           initialView='dayGridMonth'
+           height='100%'
+           headerToolbar={{
+             left: 'prev',
+             center: 'title',
+             right: 'next'
+           }} />
+         </div>
 
          <div className='streak-box'>
           <div className='streak-box-text'>
