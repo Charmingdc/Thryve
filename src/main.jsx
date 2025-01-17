2803afc81e7110f413a65816ceddf5d309d1a658
Charmingdc <charmingdc002@gmail.com>
2025-01-17 17:09:05 +0100

    minor update

--- src/main.jsx
+++ src/main.jsx
@@ -30,11 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <ThemeProvider>
      <BrowserRouter>
        <Routes>
-         <Route path='/' element={
-           <PublicRoute>
-             <App />
-           </PublicRoute>
-         } />
+         <Route path='/' element={<App />} />
          <Route path='*' element={<ErrorPage />} />
          <Route path='/login' element={<PublicRoute> 
              <LoginPage /> 
